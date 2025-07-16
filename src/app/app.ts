b0a1f9bc9fcc6feeb6f18578070362a5018ec804
Sitaram8472 <sitaramsahu8472@gmail.com>
2025-07-16 16:37:35 +0530

    update all

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,16 +1,26 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from "./components/header/header";
+import { Home } from "./home/home";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  template: `
-    <h1>Welcome to {{ title() }}!</h1>
+  imports: [RouterOutlet, Header, Home],
+ template: `
 
-    <router-outlet />
-  `,
+   <h1>Welcome to {{ title() }}!</h1>
+
+
+  <app-header/>
+  <app-home/>
+  <router-outlet />
+`,
   styles: [],
 })
+
+
+
+
 export class App {
   protected readonly title = signal('JIS_app');
 }
